3c8440663dd35e85674df9bada1c91129f5cda20
mihael5 <michael.eliad@gmail.com>
2020-06-21 15:18:20 +0300

    Constructor, getters, isBefore

--- Date.java
+++ Date.java
@@ -4,7 +4,14 @@ public class Date {
   private int day;
 
   public Date(int day, int month, long year){
-    //you fill here
+    if(day < 31 && month < 13){
+      this.day = day;
+      this.month = month;
+      this.year = year;
+    }
+    else{
+      System.out.println("Error");
+    }
   }
 
   public boolean equals(Date other) {
@@ -12,14 +19,28 @@ public class Date {
   }
 
   public String toString(){
-    return null; //you write here
+    return ("Year: " + this.getYear() + ". \n" + "Month: " + this.getMonth() + ". \n" + "Day: " + this.getDay() + ".");
   }
 
-  public int getDay(){}
-  public int getMonth(){}
-  public long getYear(){}
+  public int getDay(){
+    return this.day;
+  }
+  public int getMonth(){
+    return this.month;
+  }
+  public long getYear(){
+    return this.year;
+  }
 
-  public boolean isBefore(Date other){  }
+  public boolean isBefore(Date other) {
+    boolean answer = false;
+    if (this.getYear()<other.getYear()) {answer = true}
+    else if (this.getYear()==other.getYear() && this.getMonth()<other.getMonth()){answer = true}
+    else if (this.getYear()==other.getYear() && this.getMonth()==other.getMonth() && this.getDay()<other.getDay()){answer = true}
+    return answer;
+
+    // return other.getYear() > this.year || (other.getYear() == this.year && (other.getMonth() > this.month || (other.getMonth() == this.month && other.getDay() > this.day)));
+  }
 
 
   //setters
