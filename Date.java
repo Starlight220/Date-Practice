9fdf6867f4a260788d33ffd1c6d26fe5733b9cc0
Starlight220 <yotamshlomi@gmail.com>
2020-06-21 14:29:24 +0300

    added stubs

--- Date.java
+++ Date.java
@@ -2,4 +2,26 @@ public class Date {
   private long year;
   private int month;
   private int day;
+
+  public Date(int day, int month, long year){
+    //you fill here
+  }
+
+  public boolean equals(Date other) {
+    return false; //you write here (this == other)
+  }
+
+  public String toString(){
+    return null; //you write here
+  }
+
+  public int getDay(){}
+  public int getMonth(){}
+  public long getYear(){}
+
+  public boolean isBefore(Date other){  }
+
+
+  //setters
+
 }
